1e3ab926709b647123a87f0edf978762efa00308
Xu Liangwei <xlwyes@gmail.com>
2016-04-09 12:57:11 +0800

    add join event

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,18 @@ let io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 
 io.on('connection', socket => {
-  let _room = socket.conn.request._query.url
-  let _email = null
-  console.log(`${socket.id} connect to room ${_room}`)
-  socket.join(_room)
+  let _room = null
+
+  socket.on('join', (url) => {
+    _room = url
+    console.log(`${socket.id} connect to room ${_room}`)
+    socket.join(_room)
+  })
 
   socket.on('scroll', ({location, username, email}) => {
     groups[_room] = groups[_room] ? groups[_room] : {}
-    _email = email
-    if (!groups[_room][email]) {
-      groups[_room][email] = {
+    if (!groups[_room][socket.id]) {
+      groups[_room][socket.id] = {
         location: location,
         username: username,
         email: email,
@@ -34,8 +36,8 @@ io.on('connection', socket => {
   })
 
   socket.on('disconnect', () => {
-    if (groups[_room][_email]) {
-      delete groups[_room][_email]
+    if (groups[_room][socket.id]) {
+      delete groups[_room][socket.id]
     }
     io.to(_room).emit('users', groups[_room])
   })
