4fce2de8ce31fd78b9bc9b6269ccb66298bc2ff5
Xu Liangwei <xlwyes@gmail.com>
2016-04-09 18:30:26 +0800

    add successfilly save appearence

--- frontend/chrome_extention/popup.js
+++ frontend/chrome_extention/popup.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('form').addEventListener('submit', function(e) {
     chrome.storage.sync.set({name: this.name.value, email: this.email.value})
+    document.querySelector('label').appendChild(document.createTextNode('Successfully saved!'))
+    document.querySelector('label').style = "color: red"
     e.preventDefault()
   })
   chrome.storage.sync.get(['name', 'email'], function(ret) {
