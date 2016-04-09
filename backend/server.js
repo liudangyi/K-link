4c8c42a7bc24e49c661c3072bf9b67736ec8d8ff
Xu Liangwei <xlwyes@gmail.com>
2016-04-09 13:05:37 +0800

    fix sth

--- backend/server.js
+++ backend/server.js
@@ -12,30 +12,20 @@ io.on('connection', socket => {
     _room = url
     console.log(`${socket.id} connect to room ${_room}`)
     socket.join(_room)
+    groups[_room] = groups[_room] && {}
   })
 
-  socket.on('scroll', ({location, username, email}) => {
-    groups[_room] = groups[_room] ? groups[_room] : {}
-    if (!groups[_room][socket.id]) {
-      groups[_room][socket.id] = {
-        location: location,
-        username: username,
-        email: email,
-      }
-    }
+  socket.on('scroll', param => {
+    groups[_room][socket.id] = param
     io.to(_room).emit('users', groups[_room])
   })
 
-  socket.on('message', ({message, username, email}) => {
-    io.to(_room).emit('message', {
-      username: username,
-      email: email,
-      message: message,
-    })
-    console.log(`${username} sent '${message}' to room ${_room}`)
+  socket.on('message', param => {
+    io.to(_room).emit('message', param)
   })
 
   socket.on('disconnect', () => {
+    console.log(_room)
     if (groups[_room][socket.id]) {
       delete groups[_room][socket.id]
     }
