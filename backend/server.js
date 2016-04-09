3154466c4905df92330a9f60e3d138d1bdb081c8
Xu Liangwei <xlwyes@gmail.com>
2016-04-09 11:18:23 +0800

    rewrite some events

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,43 @@ let groups = {}
 let server = require('http').createServer()
 let io = require('socket.io')(server)
 
+const port = process.env.PORT || 3000
+
 io.on('connection', socket => {
-  let room = null
-  console.log(`${socket.id} connected`)
-  socket.on('join', ([url, user]) => {
-    room = url
-    socket.join(room)
-    console.log(`${user} joined room ${room}`)
-    groups[room] = groups[room] ? groups[room] : {}
-    groups[room][socket.id] = user
-    io.to(room).emit('list users', groups[room])
-    console.log(groups[room])
+  let _room = socket.conn.request._query.url
+  let _email = null
+  console.log(`${socket.id} connect to room ${_room}`)
+  socket.join(_room)
+
+  socket.on('scroll', ({location, username, email}) => {
+    groups[_room] = groups[_room] ? groups[_room] : {}
+    _email = email
+    if (!groups[_room][email]) {
+      groups[_room][email] = {
+        location: location,
+        username: username,
+        email: email,
+      }
+    }
+    io.to(_room).emit('users', groups[_room])
   })
+
+  socket.on('message', ({message, username, email}) => {
+    io.to(_room).emit('message', {
+      username: username,
+      email: email,
+      message: message,
+    })
+    console.log(`${username} sent '${message}' to room ${_room}`)
+  })
+
   socket.on('disconnect', () => {
-    console.log(`${socket.id} disconnected`)
-    delete groups[room][socket.id]
-    io.to(room).emit('list users', groups[room])
+    if (groups[_room][_email]) {
+      delete groups[_room][_email]
+    }
+    io.to(_room).emit('users', groups[_room])
   })
 })
 
-server.listen(3000)
+server.listen(port)
+console.log(`listening on port ${port}`)
